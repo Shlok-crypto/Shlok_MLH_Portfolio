8c9010fc5b12db0784e3bcd962e70db0faf4b4e8
mnehra <32586131+mnehra@users.noreply.github.com>
2025-07-09 12:49:39 -0700

    About message

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    work_experiences = [
+        {"role": "Software Engineer Intern", "company": "Company A", "duration": "Summer 2024"},
+        {"role": "Junior Developer", "company": "Company B", "duration": "2022 - 2023"}
+    ]
+    return render_template('index.html', experiences=work_experiences)
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
 if __name__ == '__main__':
