f52b203a7aa8991351e4acd6dab1da725dadccbf
Shlok Tomar <83566027+Shlok-crypto@users.noreply.github.com>
2025-07-28 12:28:02 -0700

    Create redeploy-site.sh
    Added Docker container

---
+++ redeploy-site.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e  # Exit immediately if any command fails
+
+# Navigate to your project folder
+cd /root/Shlok_MLH_Portfolio
+
+# Pull latest changes from GitHub main branch
+git fetch && git reset origin/main --hard
+
+# Stop existing containers
+docker compose -f docker-compose.prod.yml down
+
+# Start containers again, rebuilding if needed
+docker compose -f docker-compose.prod.yml up -d --build
